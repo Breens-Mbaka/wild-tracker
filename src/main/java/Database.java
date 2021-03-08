53423796fbc51679a38613d40f63da93465bed1e
Breens-Thomas <mbakabreens@gmail.com>
2021-03-08 10:20:28 +0300

    Added databasefor app

---
+++ src/main/java/Database.java
@@ -0,0 +1,9 @@
+import org.sql2o.Sql2o;
+
+public class Database{
+    public static Sql2o sql2o  = new Sql2o("jdbc:postgresql://ec2-3-208-224-152.compute-1.amazonaws.com:5432/d6tclvuu3g9tdh", "ujifyjjserbuwf", "d4ff9209243294046fa67653d5bdd6199190bd01685275cce61b55e19f77b46b");
+
+}
+
+
+
